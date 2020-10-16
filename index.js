f634073819814ad0379353d956fa30568c2a7c1d
Alexander Quintana <alesanderamador@gmail.com>
2020-10-16 20:51:32 +0000

    hola

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-he
+const express = require ("express");
+const app = express();
+
+app.get("/",function(req,rest){
+  rest.send("Mi servidor de numeros pares")
+})
+
+app.get("/parimpar/:numero",function(req,rest){
+  let numero = req.params.numero
+
+  if ( numero %2 ==0)
+  rest.send ("el numero es par")
+  else 
+  rest.send("el numero es impar")
+
+  
+
+  
+})
+
+
+app.listen(8080,function(){
+  console.log("servidor escuchando en el puerto 8080")
+})
